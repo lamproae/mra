d807595cdd0f082407f6005abd085288fb1bfa83
lamproae <lwnope@163.com>
2017-06-03 22:20:33 -0700

    Add more

--- server/basic.go
+++ server/basic.go
@@ -7,8 +7,43 @@ import (
 	"net/http"
 )
 
+type Page struct {
+	Link        string
+	Description string
+}
+
 func MainPage(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("template/index.html", "template/footer.html", "template/header.html")
+	if err != nil {
+		log.Println(err)
+		io.WriteString(w, err.Error())
+		return
+	}
+
+	data := struct {
+		Title string
+		Pages []Page
+	}{
+		Title: "Welcom to ATS System",
+		Pages: []Page{
+			{Link: "/", Description: "Main Page"},
+			{Link: "/bootstrap", Description: "bootstrap test page"},
+			{Link: "/invalid", Description: "Invalid http request page."},
+			{Link: "/notfind", Description: "This should be the 404 page."},
+			{Link: "/inputtest", Description: "This is a page for test html form."},
+			{Link: "/registernewcase", Description: "Register a new ATS case."},
+			{Link: "/index", Description: "This is also the main page."},
+		},
+	}
+
+	err = t.Execute(w, data)
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
+
+func BootStrap(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("template/bootstrap.html", "template/footer.html", "template/header.html")
 	if err != nil {
 		log.Println(err)
 		io.WriteString(w, err.Error())
@@ -39,23 +74,75 @@ func RedirectToMain(w http.ResponseWriter, r *http.Request) {
 }
 
 func RegisterNewCase(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("template/registernewcase.html", "template/footer.html", "template/header.html")
-	if err != nil {
-		log.Println(err)
-		io.WriteString(w, err.Error())
-		return
-	}
+	log.Println(r.Method)
+	if r.Method == "GET" {
+		t, err := template.ParseFiles("template/registernewcase.html", "template/footer.html", "template/header.html")
+		if err != nil {
+			log.Println(err)
+			io.WriteString(w, err.Error())
+			return
+		}
 
-	err = t.Execute(w, nil)
-	if err != nil {
-		log.Println(err.Error())
+		err = t.Execute(w, nil)
+		if err != nil {
+			log.Println(err.Error())
+		}
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println("Cannot parse form: ", err.Error())
+			return
+		}
+
+		log.Println(r.Form)
+	} else {
+		http.Redirect(w, r, "/invalid", http.StatusTemporaryRedirect)
 	}
+}
+
+func InputTest(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.Method)
+	if r.Method == "GET" {
+		t, err := template.ParseFiles("template/inputtest.html", "template/footer.html", "template/header.html")
+		if err != nil {
+			log.Println(err)
+			io.WriteString(w, err.Error())
+			return
+		}
+
+		err = t.Execute(w, nil)
+		if err != nil {
+			log.Println(err.Error())
+		}
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println("Cannot parse form: ", err.Error())
+			return
+		}
+
+		log.Println(r.Form)
+	} else {
+		http.Redirect(w, r, "/invalid", http.StatusTemporaryRedirect)
+	}
+}
+
+func ResourceNotFoundHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func InvalidReqMethodHandler(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func main() {
 	//@liwei: This need more analysis.
 	http.HandleFunc("/", RedirectToMain)
 	http.HandleFunc("/index", MainPage)
+	http.HandleFunc("/notfind", ResourceNotFoundHandler)
+	http.HandleFunc("/invalid", InvalidReqMethodHandler)
+	http.HandleFunc("/bootstrap", BootStrap)
+	http.HandleFunc("/inputtest", InputTest)
 	http.HandleFunc("/registernewcase", RegisterNewCase)
 	http.Handle("/static/", http.FileServer(http.Dir(".")))
 	http.ListenAndServe(":8080", nil)
--- server/static/js/registernewcase.js
+++ server/static/js/registernewcase.js
@@ -1,137 +1,49 @@
-$(document).ready(function(){
-	alert("Hello world")
-	$(function(){
-		$('.team-list').on("click",".close-div-button",function(){
-			if($(this).parents('.team-details').find('input[name^=team_id]').val()!='')
-			{
-				var id=$(this).parents('.team-details').find('input[name^=team_id]').val();
-				var r=confirm('Are you sure you want to delete this experience details?');
-				if(r==true)
-				{
-					$(this).parents('.team-details').remove();
-					$.ajax({
-						url: "/index.php/creative/delete_team",
-						type:"post",
-						data:{"id":id},
-						success:function(data){
-							console.log(data);
-						}
-					});
-					$('.team-list').find('.team-details').last().css("border-bottom","0px");
-				}
-				else if(r==false){
-					return;
-				}
-			}
-			else{
-
-				$(this).parents('.team-details').remove();
-				$('.team-list').find('.team-details').last().css("border-bottom","0px");
-
-			}	
-		});
-
-
-		$('.team-list').on("click",".reset-button",function(){
-			var team_container=$(this).parents('.team-details');
-			console.log(team_container);
-			$(team_container).find("input").not("input[name^=team_id]").val('');
-			$(team_container).find("textarea").val('');
-			$(team_container).find("select").each(function(){
-				$('option:selected',this).removeAttr('selected');
-			});
-			$(team_container).find('.gmaps-output-latitude').html('');
-			$(team_container).find('.gmaps-output-longitude').html('');
-			$(team_container).find("select[name='from_month[]'] option[value='January']").attr("selected","selected");
-			$(team_container).find("select[name='from_year[]'] option[value='2016']").attr("selected","selected");
-			$(team_container).find("select[name='to_month[]'] option[value='January']").attr("selected","selected");
-			$(team_container).find("select[name='to_year[]'] option[value='2021']").attr("selected","selected");
-			$(team_container).find('.to').next().removeClass('inlineBlock');
-			$(team_container).find('.to').next().addClass('hidden-div');
-			$(team_container).find('.current-position').hide();
-			$(team_container).find('.current-field-checkbox').show();
-			$(team_container).find('.to-month').show();
-			$(team_container).find('.to-year').show();
-			$(team_container).find("input[type=checkbox]").prop("checked",false);
-			$(team_container).find('.tagit-choice').remove();
-		});
-
-
-		var i=0;var a=0;		
-		var country="";
-		var state="";
-		$('select[name=country]').val(country);
-		$('select[name=state]').val(state);
-		$('.country').each(function(){
-			if($(this).val()!='India')
-			{
-				$(this).parents('.individual-fields').siblings('.state').hide();
-			}
-		});
-		if($('#location-country').val()!="India")
-		{
-			$('#location-state').hide();
-		}
-		else{
-			$('select[name=state]').addClass('required');
-		}		
-	});
-
-	var i=0;
-	$('.teams').on("click",function(){
-		i++;
-		console.log('cp1');
-		var team_div	=$('.team-details')[0];
-		var cloned_team_div=$(team_div).clone();
-		$(cloned_team_div).find("input").val('')
-		$(cloned_team_div).find('.tagit-choice,.tagit-new').remove();
-		$('.team-details').css("border-bottom","1px solid #DCE0E0");
-		$('.team-details').css("margin-bottom","20px");
-		$(cloned_team_div).css("border-bottom","0px");
-		$(cloned_team_div).find("textarea").html('');
-		$(cloned_team_div).find("textarea").val('');
-		$(cloned_team_div).find("select").each(function(){
-			var value=$(this).val();
-			$('option:selected',this).removeAttr('selected');
-		});	
-
-		$(cloned_team_div).find('.current-position').removeClass('inlineBlock');
-		$(cloned_team_div).find('.current-position').addClass('hidden-div');
-		$(cloned_team_div).find("select[name='from_month[]'] option[value='January']").attr("selected","selected");
-		$(cloned_team_div).find("select[name='from_year[]'] option[value='2015']").attr("selected","selected");
-		$(cloned_team_div).find("select[name='to_month[]'] option[value='January']").attr("selected","selected");
-		$(cloned_team_div).find("select[name='to_year[]'] option[value='2015']").attr("selected","selected");
-		\
-		$(cloned_team_div).find('.tags-div').show();
-		$(cloned_team_div).find('.current-position').removeClass('inlineBlock');
-		$(cloned_team_div).find('.current-position').addClass('hidden-div');
-		$(cloned_team_div).find('.to-month').css("display","inline-block");
-		$(cloned_team_div).find('.to-year').css("display","inline-block");
-		$(cloned_team_div).find('.close-div-button').show();
-		$(cloned_team_div).find('.reset-button').hide();
-		$(cloned_team_div).find('input[type=checkbox]').prop("checked",false);
-		$(cloned_team_div).find('input[type=checkbox]').val()==0;
-
-		$(cloned_team_div).clone().appendTo('.team-list');
-
-		$('.team-list').on("click","input[type=checkbox]",function(){
-			if($(this).prop("checked"))
-			{
-				console.log("cp1");
-				$(this).parents('.experience-checkbox').siblings('.to-month').hide();
-				$(this).parents('.experience-checkbox').siblings('.to-year').hide();
-
-				$(this).parents('.experience-checkbox').siblings('.current-position').removeClass('hidden-div');
-				$(this).parents('.experience-checkbox').siblings('.current-position').addClass('inlineBlock');
-			}
-			else
-			{
-				console.log("cp2");
-				$(this).parents('.experience-checkbox').siblings('.current-position').addClass('hidden-div');
-				$(this).parents('.experience-checkbox').siblings('.current-position').removeClass('inlineBlock');
-				$(this).parents('.experience-checkbox').siblings('.to-month').show();
-				$(this).parents('.experience-checkbox').siblings('.to-year').show();
-			}
-		});
-	});
+var next = 1;
+$(".add-more").click(function(e){
+	e.preventDefault();
+	var addto = "#field" + next;
+	next = next + 1;
+	var newIn = '<br /><br /><input autocomplete="off" class="span3" id="field' + next + '" name="field' + next + '" type="text" data-provide="typeahead" data-items="8">';
+	var newInput = $(newIn);
+	$(addto).after(newInput);
+	$("#field" + next).attr('data-source',$(addto).attr('data-source'));
+	$("#count").val(next);  
 });
+
+$("#remove").click(function(){
+	$("#table").remove();
+});
+
+$("#add").click(function(){
+	$("#table").clone().appendTo("#div1");
+});
+
+var i=1;
+$("#add_row").click(function(){
+	$('#addr'+i).html("<td align = 'center'><img src = 'http://www.placehold.it/200x200' alt = '...'></img></td><td><input name='emotion"+i+"' type='text' placeholder='Emotion' class='form-control input-md'/> </td><td><input  name='source"+i+"' type='text' placeholder='Source'  class='form-control input-md'></td>");
+
+	$('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+	i++; 
+});
+
+$("#delete_row").click(function(){
+	if(i>1){
+		$("#addr"+(i-1)).html('');
+		i--;
+	}
+});
+
+var i=1;
+$("#add_row1").click(function(){
+	$('#addr'+i).html("<td>"+ (i+1) +"</td><td><input name='name"+i+"' type='text' placeholder='Name' class='form-control input-md'  /> </td><td><input  name='mail"+i+"' type='text' placeholder='Mail'  class='form-control input-md'></td><td><input  name='mobile"+i+"' type='text' placeholder='Mobile'  class='form-control input-md'></td>");
+
+	$('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+	i++; 
+});
+$("#delete_row1").click(function(){
+	if(i>1){
+		$("#addr"+(i-1)).html('');
+		i--;
+	}
+});
+
