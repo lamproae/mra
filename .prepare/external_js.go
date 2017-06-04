a67de6057d56b7d193e57bf0b87849b1b1596a23
lamproae <lwnope@163.com>
2017-06-03 17:33:11 -0700

    Add more http test

---
+++ .prepare/external_js.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+)
+
+var tpl = `<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>{{.Title}}</title>
+		<script type="text/javascript" src="static/js/demo.js"></script>
+	</head>
+	</body>
+		{{range .Items}}
+			<div>{{.}}</div>
+		{{else}}
+			<div>no row</div>
+		{{end}}
+	</body>
+</html>`
+
+var data = struct {
+	Title string
+	Items []string
+}{
+	Title: "First Page",
+	Items: []string{
+		"My photos",
+		"My movie",
+		"My video",
+		"My audio",
+	},
+}
+
+func MainPage(w http.ResponseWriter, r *http.Request) {
+	t, err := template.New("MainPage").Parse(tpl)
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+
+	err = t.Execute(w, data)
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", MainPage)
+	http.Handle("/static/", http.FileServer(http.Dir(".")))
+	http.ListenAndServe(":8080", nil)
+}
